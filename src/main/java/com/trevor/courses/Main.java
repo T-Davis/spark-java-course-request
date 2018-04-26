7ed874dcd16104497c72838542abc191c4b48d1b
Trevor Davis <tzendou@gmail.com>
2018-04-25 23:37:17 -0700

    Add root ('/') uri @ localhost:4567 and Handlebars templating engine

--- src/main/java/com/trevor/courses/Main.java
+++ src/main/java/com/trevor/courses/Main.java
@@ -1,9 +1,15 @@
 package com.trevor.courses;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 import static spark.Spark.get;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+
+        // index.hbs file is in resources/templates directory
+        get("/", (rq, rs) -> new ModelAndView(null, "index.hbs"), new HandlebarsTemplateEngine());
+
     }
 }
